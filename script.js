20d1a87fdef75df23fbf869d8e9e457a44f84e9e
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-22 02:26:01 +0800

    Loop grid createElement for 16*16

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-for (let i = 1; i <= 16 * 16; i++) {}
+for (let i = 1; i <= 16 * 16; i++) {
+  const container = document.querySelector("#grid-container");
+  const grid = document.createElement("div");
+  grid.classList = "grid";
+  container.appendChild(grid);
+}
