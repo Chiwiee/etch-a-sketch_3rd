0954030170acb52b1041bdc2c096abfeac759f89
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-06-22 04:20:32 +0800

    Change to EventListerner and move getValue function inside

--- script.js
+++ script.js
@@ -15,13 +15,15 @@ grid.forEach((grid) => {
   });
 });
 
-function getValue() {
-  const input = prompt("Pick between: 32x32 or 64x64");
-  if (input == 32) {
-    return value * 2;
-  } else if (input == 64) {
-    return value * 4;
-  }
-}
 const button = document.querySelector("#button");
-button.onclick = () => getValue();
+button.addEventListener("click", () => {
+  function getValue() {
+    const input = prompt("Pick between: 32x32 or 64x64");
+    if (input == 32) {
+      return value * 2;
+    } else if (input == 64) {
+      return value * 4;
+    }
+  }
+  const input = getValue();
+});
